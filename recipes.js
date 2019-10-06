0735ef50aa354be193aae83617e7b26e197a7c10
Daniel Keehn <danielkeehn@bellsouth.net>
2019-10-06 00:34:37 -0400

    Recipes.js organizes the recipes into arrays

---
+++ recipes.js
@@ -0,0 +1,9 @@
+//This file organizes all the recipies in structs
+
+var eggsRecipe = [{"Egg": 2}];
+var chocolateStrawberriesRecipe = [{"Chocolate": 3}, {"Strawberry": 3}];
+var cookiesRecipe = [{"Sugar": 3}, {"Flour": 1}, {"Milk": 2}, {"Egg": 2}];
+var browniesRecipe = [{"Sugar": 4}, {"Flour": 2}, {"Milk": 4}, {"Egg": 4}, {"Chocolate": 6}];
+var applePieRecipe = [{"Sugar": 6}, {"Flour": 1}, {"Milk": 1}, {"Egg": 2}, {"Apple": 12}];
+var cupCakeRecipe = [{"Sugar": 0}, {"Flour": 0}, {"Milk": 0}, {"Egg": 0}, {"Chocolate": 0},{"Icing": 0}];
+var cakeRecipe = [{"Sugar": 0}, {"Flour": 0}, {"Milk": 0}, {"Egg": 0}, {"Chocolate": 0},{"Icing": 0},{"Strawberry": 0},{"Cherry": 0}]; 
